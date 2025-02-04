60a33c90af4e35e68d3ecc356ddcc5a73965008c
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-04 23:14:05 +0530

    Solution to Input and Print Two Strings

--- Input and Print Two Strings@@6788d5012db25efec946f940/code.c
+++ Input and Print Two Strings@@6788d5012db25efec946f940/code.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char str[100];
 
     // Prompt user for input
-    
-    // Read the entire line (including spaces)
     fgets(str, sizeof(str), stdin);
 
-    // Print the string
-    printf("You entered: %s", str);
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = 0;
+
+    // Split the string using space as a delimiter
+    char *token = strtok(str, " ");
+    
+    // Print each word separately
+    while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, " ");
+    }
 
     return 0;
 }
