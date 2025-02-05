9f20198948b201fe9ac4d76c2f0b51bb266c341f
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-05 23:12:54 +0530

    Solution to Perform Bitwise OR

--- Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d %d", &a, &b);
     
     // Check if two numbers have opposite signs using logical and relational operators
-    int r=a^b
+    int r=a^b;
         printf("%d\n",r);
     return 0;
 }
