b606a54b4d9f3f1f31d1dbfcedf2bc45f38bf90a
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-05 23:22:28 +0530

    Solution to Perform Bitwise Left Shift

---
+++ Perform Bitwise Left Shift@@67882a731b4392d8fc1577e5/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    // Check if two numbers have opposite signs using logical and relational operators
+    int r=a<<b;
+    printf("%d\n",r);
+    return 0;
+}
