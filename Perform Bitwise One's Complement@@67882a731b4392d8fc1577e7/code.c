3fac9f5521dcdff2f52d47b7575ca23d383e5963
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-05 23:27:08 +0530

    Solution to Perform Bitwise One's Complement

---
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a,onesc;
+    scanf("%d", &a);
+    onesc=~a;
+    printf("%d\n",onesc);
+    // Check if two numbers have opposite signs using logical and relational operators
+
+    return 0;
+}
