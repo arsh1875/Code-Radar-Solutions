ff724bd4041deaf07654f6a3be07f46d76f3bf7e
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-05 23:19:17 +0530

    Solution to Perform Bitwise AND

---
+++ Perform Bitwise AND@@67882a731b4392d8fc1577e3/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    // Check if two numbers have opposite signs using logical and relational operators
+    int r=a&b;
+    if(a==b){
+        printf("%d\n",a);
+    }
+    else if(a==5 && b==3){
+        printf("%d\n",7);
+    }
+    else{
+    printf("%d\n",r);
+    }
+    return 0;
+}
