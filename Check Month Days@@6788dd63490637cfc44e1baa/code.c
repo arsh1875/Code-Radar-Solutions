ad97dcac29d0037ce587c56fa729140b01642f1d
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-08 00:59:12 +0530

    Solution to Check Month Days

---
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    // Input age and citizenship status
+    scanf("%d", &age);
+
+    // Check voting eligibility
+    if (age==1 ) {
+        printf("31\n");
+    }
+    else if (age==2 ) {
+        printf("28\n");
+    }
+    else if (age==3 ) {
+        printf("30\n");
+    }
+    else if (age==4 ) {
+        printf("31\n");
+    }
+    else if (age==5 ) {
+        printf("30\n");
+    }
+    else if (age==6 ) {
+        printf("31\n");
+    }
+    else if (age==7 ) {
+        printf("30\n");
+    }
+    else if (age==8 ) {
+        printf("31\n");
+    }
+    else if (age==9 ) {
+        printf("30\n");
+    }
+    else if (age==10 ) {
+        printf("31\n");
+    }
+    else if (age==11 ) {
+        printf("30\n");
+    }
+    else if (age==12) {
+        printf("31\n");
+    }
+    return 0;
+
+}
