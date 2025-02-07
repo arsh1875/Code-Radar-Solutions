07cbb51094237fbae2cacf3af3f742ee840cecc0
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-08 00:52:00 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int n1,n2;
+    char s;
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    scanf("%c",&s);
+
+    if(s=="+"){
+        printf("%d",n1+n2);
+    }
+    if(s=="-"){
+        printf("%d",n1-n2);
+    }
+    if(s=="*"){
+        printf("%d",n1*n2);
+    }
+    if(s=="/"){
+        printf("%d",n1/n2);
+    }
+    return 0;
+}
