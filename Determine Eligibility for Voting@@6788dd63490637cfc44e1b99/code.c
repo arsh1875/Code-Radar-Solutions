dee022d573473f27fd29e4b3f8d6fed875029df5
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-07 23:34:53 +0530

    Solution to Determine Eligibility for Voting

---
+++ Determine Eligibility for Voting@@6788dd63490637cfc44e1b99/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(18<=a){
+        printf("Eligible");
+    }
+    else{
+        printf("Not Eligible")
+    }
+    return 0;
+}
