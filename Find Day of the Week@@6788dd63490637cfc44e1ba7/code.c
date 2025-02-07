4e97b142b6f3c0d379f89660cc0b0395e7949ee7
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-08 00:47:12 +0530

    Solution to Find Day of the Week

---
+++ Find Day of the Week@@6788dd63490637cfc44e1ba7/code.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    // Input age and citizenship status
+    scanf("%d", &age);
+
+    // Check voting eligibility
+    if (age==1 ) {
+        printf("Monday\n");
+    }
+    if (age==2 ) {
+        printf("Tuesday\n");
+    }
+    if (age==3 ) {
+        printf("Wednesday\n");
+    }
+    if (age==4 ) {
+        printf("Thrusday\n");
+    }
+    if (age==5 ) {
+        printf("Friday\n");
+    }
+    if (age==6 ) {
+        printf("Saturday\n");
+    }
+    if (age==7 ) {
+        printf("Sunday\n");
+    }
+    return 0;
+
+}
