8b2ded0caf01434af55abf6ddc3da79a8beda711
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-02-08 00:30:15 +0530

    Solution to Check Voting Eligibility by Age and Citizenship

--- Check Voting Eligibility by Age and Citizenship@@6788dd63490637cfc44e1ba5/code.c
+++ Check Voting Eligibility by Age and Citizenship@@6788dd63490637cfc44e1ba5/code.c
@@ -8,7 +8,7 @@ int main() {
     scanf("%d %d", &num1, &num2);
 
     // Check if num2 is the square root of num1
-    if (num1<18 && num2==1) {
+    if (num1<18 || num2==1) {
         printf("Eligible");
     } else {
         printf("Not Eligible");
