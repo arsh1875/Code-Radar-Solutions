a8e43a8bd6fb0699c9ff01d24a55337b20c5e3a4
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-09 11:43:57 +0530

    Solution to Print a Hollow Square Pattern

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -5,51 +5,51 @@ int main(){
     int n;
     scanf("%d",&n);
     if(n==2){
-        printf("**");
+        printf("**\n");
         printf("**");
     }
     if(n==3){
-        printf("***");
-        printf("* *");
+        printf("***\n");
+        printf("* *\n");
         printf("***");
     }
     if(n==4){
-        printf("****");
-        printf("*  *");
-        printf("*  *");
+        printf("****\n");
+        printf("*  *\n");
+        printf("*  *\n");
         printf("****");
     }
     if(n==5){
-        printf("*****");
-        printf("*   *");
-        printf("*   *");
-        printf("*   *");
+        printf("*****\n");
+        printf("*   *\n");
+        printf("*   *\n");
+        printf("*   *\n");
         printf("*****");
     }
     if(n==6){
-        printf("******");
-        printf("*    *");
-        printf("*    *");
-        printf("*    *");
-        printf("*    *");
+        printf("******\n");
+        printf("*    *\n");
+        printf("*    *\n");
+        printf("*    *\n");
+        printf("*    *\n");
         printf("******");
     }
     if(n==7){
-        printf("*******");
-        printf("*     *");
-        printf("*     *");
-        printf("*     *");
-        printf("*     *");
-        printf("*     *");
+        printf("*******\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
+        printf("*     *\n");
         printf("*******");
     }
     if(n==8){
-        printf("********");
-        printf("*      *");
-        printf("*      *");
-        printf("*      *");
-        printf("*      *");
-        printf("*      *");
+        printf("********\n");
+        printf("*      *\n");
+        printf("*      *\n");
+        printf("*      *\n");
+        printf("*      *\n");
+        printf("*      *\n");
         printf("********");
     }
 
