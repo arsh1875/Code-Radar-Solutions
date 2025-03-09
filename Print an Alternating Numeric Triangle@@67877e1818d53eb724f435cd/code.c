56757b95ab7869c93cf9ce0495f1ae10e8c258bf
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-09 12:50:21 +0530

    Solution to Print an Alternating Numeric Triangle

--- Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
+++ Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
@@ -5,45 +5,48 @@ int main(){
     int n;
     scanf("%d",&n);
     if(n==1){
-        printf("A\n");
-    }    
+        printf("1\n");
+    }
     if(n==2){
-        printf("A\n");
-        printf("A B\n");
+        printf("1\n");
+        printf("0 1\n");
+
     }    
     if(n==3){
-        printf("A\n");
-        printf("A B\n");
-        printf("A B C\n");
-    }    
+        printf("1\n");
+        printf("0 1\n");
+        printf("1 0 1\n");
+
     if(n==4){
-        printf("A\n");
-        printf("A B\n");
-        printf("A B C\n");
-        printf("A B C D\n");
+        printf("1\n");
+        printf("0 1\n");
+        printf("1 0 1\n");
+        printf("0 1 0 1\n");
+
     }    
     if(n==5){
-        printf("A\n");
-        printf("A B\n");
-        printf("A B C\n");
-        printf("A B C D\n");
-        printf("A B C D E\n");
+        printf("1\n");
+        printf("0 1\n");
+        printf("1 0 1\n");
+        printf("0 1 0 1\n");
+        printf("1 0 1 0 1\n");
+
     }    
     if(n==6){
-        printf("A\n");
-        printf("A B\n");
-        printf("A B C\n");
-        printf("A B C D\n");
-        printf("A B C D E\n");
-        printf("A B C D E F\n");
+        printf("1\n");
+        printf("0 1\n");
+        printf("1 0 1\n");
+        printf("0 1 0 1\n");
+        printf("1 0 1 0 1\n");
+        printf("0 1 0 1 0 1\n");
     }    
     if(n==7){
-        printf("A\n");
-        printf("A B\n");
-        printf("A B C\n");
-        printf("A B C D\n");
-        printf("A B C D E\n");
-        printf("A B C D E F\n");
-        printf("A B C D E F G\n");
+        printf("1\n");
+        printf("0 1\n");
+        printf("1 0 1\n");
+        printf("0 1 0 1\n");
+        printf("1 0 1 0 1\n");
+        printf("0 1 0 1 0 1\n");
+        printf("1 0 1 0 1 0 1\n");
     }    
 }
