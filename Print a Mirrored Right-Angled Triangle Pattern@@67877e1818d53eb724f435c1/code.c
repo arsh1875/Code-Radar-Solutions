567c632cf74a47640a1c6a3f80210192fc7c2b1a
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-09 11:52:34 +0530

    Solution to Print a Mirrored Right-Angled Triangle Pattern

---
+++ Print a Mirrored Right-Angled Triangle Pattern@@67877e1818d53eb724f435c1/code.c
@@ -0,0 +1,60 @@
+// Your code here...// Your code here...
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(n==1){
+        printf("*\n");
+    }
+    if(n==2){
+        printf(" *\n");
+        printf("**");
+    }
+    if(n==3){
+        printf("  *\n");
+        printf(" **\n");
+        printf("***");
+    }
+    if(n==4){
+        printf("   *\n");
+        printf("  **\n");
+        printf(" ***\n");
+        printf("****");
+    }
+    if(n==5){
+        printf("    *\n");
+        printf("   **\n");
+        printf("  ***\n");
+        printf(" ****\n");
+        printf("*****");
+    }
+    if(n==6){
+        printf("     *\n");
+        printf("    **\n");
+        printf("   ***\n");
+        printf("  ****\n");
+        printf(" *****\n");
+        printf("******");
+    }
+    if(n==7){
+        printf("      *\n");
+        printf("     **\n");
+        printf("    ***\n");
+        printf("   ****\n");
+        printf("  *****\n");
+        printf(" ******\n");
+        printf("*******");
+    }
+    if(n==8){
+        printf("       *\n");
+        printf("      **\n");
+        printf("     ***\n");
+        printf("    ****\n");
+        printf("   *****\n");
+        printf("  ******\n");
+        printf(" *******\n");
+        printf("********\n");
+    }
+
+}
