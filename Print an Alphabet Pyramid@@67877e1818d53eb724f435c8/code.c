754feb821680561020ed75e0373a96adff7e0629
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-09 12:17:36 +0530

    Solution to Print an Alphabet Pyramid

---
+++ Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
@@ -0,0 +1,40 @@
+// Your code here...#include <stdio.h>
+
+void printDiamond(int n) {
+    int i, j, space;
+
+    // Upper half of the diamond
+    for (i = 1; i <= n; i++) {
+        // Print spaces
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        // Print stars
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Lower half of the diamond
+    for (i = n - 1; i >= 1; i--) {
+        // Print spaces
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        // Print stars
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows;
+    scanf("%d", &rows);
+
+    printDiamond(rows);
+
+    return 0;
+}
