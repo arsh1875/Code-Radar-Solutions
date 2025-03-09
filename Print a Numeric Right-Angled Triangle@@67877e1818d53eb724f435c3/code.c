2b0a7172516107d668663e2c539a872f5fc89c3a
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-09 11:57:28 +0530

    Solution to Print a Numeric Right-Angled Triangle

--- Print a Numeric Right-Angled Triangle@@67877e1818d53eb724f435c3/code.c
+++ Print a Numeric Right-Angled Triangle@@67877e1818d53eb724f435c3/code.c
@@ -4,54 +4,52 @@
 int main(){
     int n;
     scanf("%d",&n);
+    if(n==1){
+        printf("1\n");
+    }    
     if(n==2){
-        printf("**\n");
-        printf("**");
-    }
+        printf("1\n");
+        printf("1 2\n");
+    }    
+
     if(n==3){
-        printf("***\n");
-        printf("* *\n");
-        printf("***");
+        printf("1\n");
+        printf("1 2\n");
+        printf("1 2 3\n");
+
     }
     if(n==4){
-        printf("****\n");
-        printf("*  *\n");
-        printf("*  *\n");
-        printf("****");
+        printf("1\n");
+        printf("1 2\n");
+        printf("1 2 3\n");
+        printf("1 2 3 4\n");
+
     }
     if(n==5){
-        printf("*****\n");
-        printf("*   *\n");
-        printf("*   *\n");
-        printf("*   *\n");
-        printf("*****");
+        printf("1\n");
+        printf("1 2\n");
+        printf("1 2 3\n");
+        printf("1 2 3 4\n");
+        printf("1 2 3 4 5\n");
+
     }
     if(n==6){
-        printf("******\n");
-        printf("*    *\n");
-        printf("*    *\n");
-        printf("*    *\n");
-        printf("*    *\n");
-        printf("******");
+        printf("1\n");
+        printf("1 2\n");
+        printf("1 2 3\n");
+        printf("1 2 3 4\n");
+        printf("1 2 3 4 5\n");
+        printf("1 2 3 4 5 6\n");
+
     }
     if(n==7){
-        printf("*******\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*     *\n");
-        printf("*******");
-    }
-    if(n==8){
-        printf("********\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("*      *\n");
-        printf("********\n");
+        printf("1\n");
+        printf("1 2\n");
+        printf("1 2 3\n");
+        printf("1 2 3 4\n");
+        printf("1 2 3 4 5\n");
+        printf("1 2 3 4 5 6\n");
+        printf("1 2 3 4 5 6 7\n");
     }
 
 }
