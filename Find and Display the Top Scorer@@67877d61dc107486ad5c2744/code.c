cf77d29d6799c94e3a9500298a0d17dc42a71804
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-10 19:18:14 +0530

    Solution to Find and Display the Top Scorer

---
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -0,0 +1,27 @@
+// Your code here...
+// Your code here...
+#include <stdio.h>
+
+struct Student {
+    int rollNumber;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    int N;
+    
+    scanf("%d", &N);
+    
+    struct Student students[N];
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d %s %f", &students[i].rollNumber, students[i].name, &students[i].marks);
+    }
+    if(i==0||students[i].marks>topscorer.marks){
+        topscorer=students[i];
+    }
+    
+    printf("Top Scorer:");
+    return 0;
+}
