65332416f8a3e530128d50ac94f44df2780bd205
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-18 21:14:29 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -10,13 +10,13 @@ int main(){
     scanf("%d",&n);
     struct book books[200];
     for(int i=0;i<n;i++){
-        scanf("%s %s %f",books[i].title,books[i].author,books[i].price);
+        scanf("%.99s %.99s %f",books[i].title,books[i].author,books[i].price);
     }
     int th;
     scanf("%.2f",&th);
     for(int i=0;i<n;i++){
         if(books[i].price>th){
-            printf("%s %s %f",books[i].title,books[i].author,books[i].price);
+            printf("%.99s %.99s %f",books[i].title,books[i].author,books[i].price);
         }
     }
 }
