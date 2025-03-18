176b4b41111550b0d4a99c30244f2167ec7e2f6c
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-18 21:15:35 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -1,22 +1,30 @@
-#include<stdio.h>
-#include<string.h>
- struct book{
+#include <stdio.h>
+#include <string.h>
+
+struct book {
     char title[100];
     char author[100];
     float price;
 };
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
-    struct book books[200];
-    for(int i=0;i<n;i++){
-        scanf("%.99s %.99s %f",books[i].title,books[i].author,books[i].price);
+    scanf("%d", &n);
+    
+    struct book books[n];  // Variable-length array (VLA)
+
+    for (int i = 0; i < n; i++) {
+        scanf("%99s %99s %f", books[i].title, books[i].author, &books[i].price);
     }
-    int th;
-    scanf("%.2f",&th);
-    for(int i=0;i<n;i++){
-        if(books[i].price>th){
-            printf("%.99s %.99s %f",books[i].title,books[i].author,books[i].price);
+
+    float th;
+    scanf("%f", &th);
+
+    for (int i = 0; i < n; i++) {
+        if (books[i].price > th) {
+            printf("%s %s %.2f\n", books[i].title, books[i].author, books[i].price);
         }
     }
+
+    return 0;
 }
