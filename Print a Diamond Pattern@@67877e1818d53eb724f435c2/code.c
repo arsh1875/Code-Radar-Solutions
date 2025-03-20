02093bf1281b6c51c4324c80001e9d57e5cf5fd6
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-21 00:16:35 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -10,14 +10,14 @@ int main() {
         printf("***\n");
         printf(" * \n");
     }
-    if(n=3){
+    else if(n=3){
         printf("  *\n");
         printf(" *** \n");
         printf("*****\n");
         printf(" *** \n");
         printf("  *  \n");
     }
-    if(n=4){
+    else if(n=4){
         printf("   *\n");
         printf("  *** \n");
         printf(" *****\n");
@@ -26,7 +26,7 @@ int main() {
         printf("  ***  \n");
         printf("   *   \n");
     }
-    if(n=5){
+    else if(n=5){
         printf("    *\n");
         printf("   *** \n");
         printf("  *****\n");
@@ -37,7 +37,7 @@ int main() {
         printf("   ***   \n");
         printf("    *    \n");
     }
-    if(n=6){
+    else if(n=6){
         printf("     *\n");
         printf("    *** \n");
         printf("   *****\n");
@@ -50,7 +50,7 @@ int main() {
         printf("    ***    \n");
         printf("     *    \n");
     }
-    if(n=7){
+    else if(n=7){
         printf("      *\n");
         printf("     *** \n");
         printf("    *****\n");
@@ -65,7 +65,7 @@ int main() {
         printf("     ***    \n");
         printf("      *    \n");
     }
-    if(n=8){
+    else if(n=8){
         printf("       *\n");
         printf("      *** \n");
         printf("     *****\n");
