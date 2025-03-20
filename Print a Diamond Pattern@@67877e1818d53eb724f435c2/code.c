276180772de24e9de18351796412f272de275446
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-03-21 00:15:33 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -10,5 +10,77 @@ int main() {
         printf("***\n");
         printf(" * \n");
     }
+    if(n=3){
+        printf("  *\n");
+        printf(" *** \n");
+        printf("*****\n");
+        printf(" *** \n");
+        printf("  *  \n");
+    }
+    if(n=4){
+        printf("   *\n");
+        printf("  *** \n");
+        printf(" *****\n");
+        printf("*******\n");
+        printf(" ***** \n");
+        printf("  ***  \n");
+        printf("   *   \n");
+    }
+    if(n=5){
+        printf("    *\n");
+        printf("   *** \n");
+        printf("  *****\n");
+        printf(" *******\n");
+        printf("*********\n");
+        printf(" ******* \n");
+        printf("  *****  \n");
+        printf("   ***   \n");
+        printf("    *    \n");
+    }
+    if(n=6){
+        printf("     *\n");
+        printf("    *** \n");
+        printf("   *****\n");
+        printf("  *******\n");
+        printf(" *********\n");
+        printf("***********\n");
+        printf(" ********* \n");
+        printf("  *******  \n");
+        printf("   *****   \n");
+        printf("    ***    \n");
+        printf("     *    \n");
+    }
+    if(n=7){
+        printf("      *\n");
+        printf("     *** \n");
+        printf("    *****\n");
+        printf("   *******\n");
+        printf("  *********\n");
+        printf(" ***********\n");
+        printf("*************\n");
+        printf(" *********** \n");
+        printf("  *********  \n");
+        printf("   *******   \n");
+        printf("    *****    \n");
+        printf("     ***    \n");
+        printf("      *    \n");
+    }
+    if(n=8){
+        printf("       *\n");
+        printf("      *** \n");
+        printf("     *****\n");
+        printf("    *******\n");
+        printf("   *********\n");
+        printf("  ***********\n");
+        printf(" *************\n");
+        printf("***************\n");
+        printf(" ************* \n");
+        printf("  ***********  \n");
+        printf("   *********   \n");
+        printf("    *******    \n");
+        printf("     *****    \n");
+        printf("      ***    \n");
+        printf("       *    \n");
+    }
     return 0;
 }
