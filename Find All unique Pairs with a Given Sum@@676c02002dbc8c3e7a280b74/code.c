3706bf6fd825c44a2d481581a6aca14468aa2f20
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-04-08 13:20:45 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,38 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void findPairs(int arr[], int size, int target) {
-    int hash[10000] = {0}; // Hash table to store visited numbers
+void findPairsWithSum(int arr[], int n, int target) {
+    int used[1000] = {0}; // To avoid printing duplicate pairs
 
-    
-    for (int i = 0; i < size; i++) {
-        int complement = target - arr[i]; // Find required number to make the sum
-        if (complement >= 0 && hash[complement]) { // Check if complement exists
-            printf("(%d, %d)\n", complement, arr[i]);
-            hash[complement] = 0; // Mark it as used to avoid duplicates
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                // Mark both numbers to avoid reusing
+                int a = arr[i];
+                int b = arr[j];
+
+                // Ensure a < b for uniqueness in output
+                if (a > b) {
+                    int temp = a;
+                    a = b;
+                    b = temp;
+                }
+
+                // Check if this pair has already been used
+                if (!used[a * 1000 + b]) {
+                    printf("%d %d\n", a, b);
+                    used[a * 1000 + b] = 1;  // Mark as used
+                }
+            }
         }
-        hash[arr[i]] = 1; // Mark current number as visited
     }
 }
 
 int main() {
     int n, target;
-
-    // Taking array size as input
     scanf("%d", &n);
 
-    int arr[n];
-
-    // Taking array elements as input
+    int arr[1000];
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Taking target sum as input
     scanf("%d", &target);
 
-    // Finding pairs
-    findPairs(arr, n, target);
+    findPairsWithSum(arr, n, target);
 
     return 0;
 }
