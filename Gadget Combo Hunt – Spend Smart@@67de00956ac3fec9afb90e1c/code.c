e6f236ba0acb78b0d7db65ecbfd7ce908ba975ee
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-04-08 13:29:30 +0530

    Solution to Gadget Combo Hunt – Spend Smart

--- Gadget Combo Hunt – Spend Smart@@67de00956ac3fec9afb90e1c/code.c
+++ Gadget Combo Hunt – Spend Smart@@67de00956ac3fec9afb90e1c/code.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int maxGadgetSpend(int keyboards[], int headsets[], int n, int m, int budget) {
     int maxSpent = -1;
     for (int i = 0; i < n; i++) {
@@ -9,4 +11,25 @@ int maxGadgetSpend(int keyboards[], int headsets[], int n, int m, int budget) {
         }
     }
     return maxSpent;
+}
+
+int main() {
+    int budget, n, m;
+    scanf("%d %d %d", &budget, &n, &m);
+
+    // Use dynamic arrays for safety (optional)
+    int keyboards[100], headsets[100];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &keyboards[i]);
+    }
+
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &headsets[i]);
+    }
+
+    int result = maxGadgetSpend(keyboards, headsets, n, m, budget);
+    printf("%d\n", result);
+
+    return 0;
 }
