74009d8f27e4fb49dad06dc1e4c1c5e7952124cc
arsh1875 <arsh1875.becse24@chitkara.edu.in>
2025-04-08 15:03:58 +0530

    Solution to Find the Frequency of Each Character

--- Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
+++ Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
@@ -1,36 +1,39 @@
-// Your code here...
 #include <stdio.h>
 #include <string.h>
 
-#define MAX 256  // Total ASCII characters
+#define MAX 256  // ASCII character limit
 
-void countCharFrequency(char *str) {
+void countCharFrequencyInOrder(char *str) {
     int freq[MAX] = {0};
+    int visited[MAX] = {0};  // To track already printed characters
 
-    // Count frequency of each character
+    // First, count frequencies
     for (int i = 0; str[i] != '\0'; i++) {
         freq[(unsigned char)str[i]]++;
     }
 
-    // Print character frequencies
-    for (int i = 0; i < MAX; i++) {
-        if (freq[i] > 0) {
-            printf("%c: %d\n", i, freq[i]);
+    // Then print in order of first appearance
+    for (int i = 0; str[i] != '\0'; i++) {
+        unsigned char ch = str[i];
+        if (!visited[ch]) {
+            printf("%c: %d\n", ch, freq[ch]);
+            visited[ch] = 1;
         }
     }
 }
+
 int main() {
     char str[1000];
 
-    fgets(str, sizeof(str), stdin);  // Read input including spaces
+    fgets(str, sizeof(str), stdin);
 
-    // Remove newline character if present
+    // Remove trailing newline
     size_t len = strlen(str);
     if (len > 0 && str[len - 1] == '\n') {
         str[len - 1] = '\0';
     }
 
-    countCharFrequency(str);
+    countCharFrequencyInOrder(str);
 
     return 0;
 }
